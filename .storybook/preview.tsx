f7681987a975e76689b81dd61595dc08fa92e963
k911 <k911@inbox.ru>
2024-11-01 15:14:12 +0300

    add:  redux Provider for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { store } from '../src/app/appStore';
 import '../src/app/main.css';
 
 const preview: Preview = {
@@ -10,6 +13,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 };
 
 export default preview;
